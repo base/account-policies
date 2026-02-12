c97ee7a9c92037bc5a3d52d9c46df07bd7c696e0
Amie Corso <amie.corso@coinbase.com>
2026-02-12 11:49:26 -0800

    update test stubs

---
+++ test/unit/policies/MorphoLoanProtectionPolicy/isPolicyUsed.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {MorphoLoanProtectionPolicyTestBase} from "../../../lib/policies/MorphoLoanProtectionPolicyTestBase.sol";
+
+/// @title isPolicyUsedTest
+///
+/// @notice Test contract for `MorphoLoanProtectionPolicy.isPolicyUsed`.
+contract isPolicyUsedTest is MorphoLoanProtectionPolicyTestBase {
+    function setUp() public {
+        setUpMorphoLoanProtectionBase(111);
+    }
+}
+
